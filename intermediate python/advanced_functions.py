0930e6cb297522ac1b06674a9797326483a25e24
pragya <pragyagc5@gmail.com>
2025-09-29 19:43:26 +0545

    lambda function

---
+++ intermediate python/advanced_functions.py
@@ -0,0 +1,14 @@
+# Normal function
+def square(x):
+    return x * x
+
+# Lambda equivalent
+square_lambda = lambda x: x * x
+
+print(square(5))         
+print(square_lambda(5))  
+
+
+#for adding two numbers
+add=lambda a,b :a+b
+print(add(2,3))
