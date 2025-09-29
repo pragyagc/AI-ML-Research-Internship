100b74a9e3b14ef2f25933d5334f478413cb1e0f
pragya <pragyagc5@gmail.com>
2025-09-29 19:43:55 +0545

    lambda functions

--- intermediate python/advanced_functions.py
+++ intermediate python/advanced_functions.py
@@ -12,3 +12,24 @@ print(square_lambda(5))
 #for adding two numbers
 add=lambda a,b :a+b
 print(add(2,3))
+
+
+
+
+#using with map,filter and sorted
+
+nums = [1, 2, 3, 4, 5]
+
+
+doubled = list(map(lambda x: x * 2, nums))
+print(doubled)  
+
+
+# Filter even numbers
+evens = list(filter(lambda x: x % 2 == 0, nums))
+print(evens) 
+
+# Sort tuples by second element
+pairs = [(1, 5), (2, 2), (3, 8)]
+sorted_pairs = sorted(pairs, key=lambda x: x[1])
+print(sorted_pairs)
