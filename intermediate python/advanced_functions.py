3189914aac301925f6e09882f42ac703d99fa621
pragya <pragyagc5@gmail.com>
2025-10-01 17:07:27 +0545

    lambda functions and closures and recursion

--- intermediate python/advanced_functions.py
+++ intermediate python/advanced_functions.py
@@ -61,3 +61,25 @@ times3 = multiplier(3)
 
 print(times2(5))
 print(times3(5)) 
+
+
+
+
+
+#recursion
+def factorial(n):
+    if n == 0 or n == 1:  
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5)) 
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print([fibonacci(i) for i in range(10)])  
+
+
