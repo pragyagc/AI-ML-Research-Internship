c7468dbb259ee74d6c4e69ca8a77f8b127bde478
pragya <pragyagc5@gmail.com>
2025-10-09 12:37:47 +0545

    class,objects and methods

---
+++ intermediate python/oop.py
@@ -0,0 +1,63 @@
+class Person:
+   #constructor initializer
+  def __init__(self,name,age):
+   self.name = name
+   self.age=age
+  def greet(self):
+    print(f"hello,my name is {self.name} and i am {self.age} years old.")
+
+#creating objects
+p1=Person("pragya",21)
+
+p1.greet()
+
+
+
+class Dog:
+  species="bulldog"
+
+  def __init__(self,name):
+   self.name=name
+
+dog1=Dog("Buddy")
+dog2=Dog("Rocky")
+
+print(dog1.name, dog1.species)
+
+
+#class and instance attributes
+class Student:
+    school_name = "ABC School"  # class attribute (shared)
+
+    def __init__(self, name):
+        self.name = name        # instance attribute (unique)
+
+s1 = Student("Pragya")
+s2 = Student("Sita")
+
+print(s1.name)          
+print(s2.name)         
+print(s1.school_name)  
+
+
+
+#methos in classes
+class Calculator:
+    def __init__(self, number):
+        self.number = number
+
+    def square(self):  # instance method
+        return self.number ** 2
+
+    @classmethod
+    def greet(cls):  # class method
+        print("Hello from Calculator class!")
+
+    @staticmethod
+    def add(a, b):  # static method
+        return a + b
+
+c = Calculator(4)
+print(c.square())           
+Calculator.greet()          # class method call
+print(Calculator.add(5, 3)) 
