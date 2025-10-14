2b59e8946907d43f21e2892f4aaf6184306e2b0e
pragya <pragyagc5@gmail.com>
2025-10-14 17:11:25 +0545

    working with matplot

--- matplot.py
+++ matplot.py
@@ -38,3 +38,80 @@ plt.title("Bar Chart Example")
 plt.xlabel("Categories")
 plt.ylabel("Values")
 plt.show()
+
+
+#piechart
+labels = ["Python", "C", "Java", "C++"]
+sizes = [40, 25, 20, 15]
+
+plt.pie(sizes, labels=labels, autopct="%1.1f%%", startangle=90)
+plt.title("Programming Language Popularity")
+plt.show()
+
+
+
+#scatter plot
+x = [5, 7, 8, 9, 10]
+y = [10, 14, 15, 20, 25]
+
+plt.scatter(x, y, color='orange')
+plt.title("Scatter Plot Example")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+plt.show()
+
+
+
+#histogram
+import numpy as np
+
+data = np.random.randn(1000)  # 1000 random numbers
+plt.hist(data, bins=20, color='purple')
+plt.title("Histogram Example")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
+
+
+#subplot(Multiple Graphs in One Figure)
+x = [1, 2, 3, 4, 5]
+y1 = [a**2 for a in x]
+y2 = [a**3 for a in x]
+
+plt.subplot(1, 2, 1)   # 1 row, 2 columns, plot 1
+plt.plot(x, y1, 'r')
+plt.title("Squares")
+
+plt.subplot(1, 2, 2)   # plot 2
+plt.plot(x, y2, 'g')
+plt.title("Cubes")
+
+plt.tight_layout()
+plt.show()
+
+
+
+#customizing graphs
+plt.plot(x, y1, color='blue', linewidth=2, marker='s', markersize=6)
+plt.title("Customized Plot", fontsize=14, color='darkred')
+plt.xlabel("X", fontsize=12)
+plt.ylabel("Y", fontsize=12)
+plt.grid(True)
+plt.show()
+
+
+
+
+
+
+
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+temp = [30, 32, 31, 29, 35, 36, 34]
+
+plt.plot(days, temp, color='orange', marker='o')
+plt.title("Temperature Over a Week")
+plt.xlabel("Days")
+plt.ylabel("Temperature (°C)")
+plt.grid(True)
+plt.show()
